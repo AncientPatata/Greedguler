06f03d15c96455e8e47a9b9b5377ab5bccf271b9
AncientPatata <abde.arrahman.dk@gmail.com>
2024-03-11 10:12:17 +0100

    Additional changes

--- data_viz.py
+++ data_viz.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         job_list = [] #[{"start_time": 0, "end_time": 5.3, "machine": 1},{"start_time": 1.05, "end_time": 3, "machine": 2}]
         for (index, machine_jobs) in enumerate(schedule):
             machine = "Machine " + str(index+1)
-            job_list += [{"job_index": job["job_index"],"start_time": datetime.fromtimestamp(job["start_time"]), "end_time": datetime.fromtimestamp(job["end_time"]), "machine": machine} for job in machine_jobs] #[dict(job, **{"machine": machine}) for job in machine_jobs[:10] ]
+            job_list += [{"job_index": job["job_index"],"start_time": datetime.fromtimestamp(job["start_time"]), "end_time": datetime.fromtimestamp(job["end_time"]), "duration": job["duration"], "machine": machine} for job in machine_jobs] #[dict(job, **{"machine": machine}) for job in machine_jobs[:10] ]
             
         df = pd.DataFrame(job_list)
         print(df.head())
--- greedguler.py
+++ greedguler.py
@@ -10,9 +10,10 @@ if __name__ == "__main__":
     # parser.add_argument('data')
     # parser.add_argument('num_machines')
     
-    dag = data_loader.load_dag_from_json("./smallComplex.json")
-    schedule = algorithm.allocate_jobs_to_machines_mod(dag)
-    print(schedule)
+    #dag = data_loader.load_dag_from_json("./smallComplex.json")
+    dag = data_loader.generate_random_dag(12,86)
+    schedule = algorithm.allocate_jobs_to_machines_mod(dag ,num_machines=3)
+    #print(schedule)
     with open("schedule.json", "w") as file_handle:
         json.dump(schedule, file_handle)
     #algorithm.pretty_print_allocated_jobs(schedule)
