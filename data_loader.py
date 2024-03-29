f708084aba908b36382296a645d657bb36d605fe
ZinanHAN <161853280+ZinanHAN@users.noreply.github.com>
2024-03-29 15:48:40 +0100

    comment

--- data_loader.py
+++ data_loader.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import networkx as nx
 import matplotlib.pyplot as plt
 from networkx.drawing.nx_pydot import graphviz_layout
@@ -8,6 +9,17 @@ import timeit
 import rustworkx as rx
 
 def generate_random_dag(num_nodes, max_duration, density_level=2):
+    '''
+    Generates a random directed acyclic graph (DAG).
+
+    Args:
+        num_nodes (int): Number of nodes in the DAG.
+        max_duration (int): Maximum duration for node execution.
+        density_level (int, optional): Density level for adding edges. Defaults to 2.
+
+    Returns:
+        nx.DiGraph: Randomly generated DAG.
+    '''
     # Create a directed acyclic graph (DAG)
     dag = nx.DiGraph()
 
@@ -24,39 +36,57 @@ def generate_random_dag(num_nodes, max_duration, density_level=2):
     return dag
 
 def plot_dag(dag):
+    '''
+    Plots the given DAG.
+
+    Args:
+        dag (nx.DiGraph): Directed acyclic graph.
+    '''
     pos =  graphviz_layout(dag, prog="dot")
-    #durations = {node: dag.nodes[node]['duration'] for node in dag.nodes}
-
     nx.draw(dag, pos, with_labels=False, node_size=8, node_color='skyblue')
-    #nx.draw_networkx_edge_labels(dag, pos, edge_labels={(i, j): f"{durations[i]} units" for i, j in dag.edges})
-
     plt.title("DAG with Random Durations")
-    #plt.savefig("dag.png")
     plt.show()
 
 def load_dag_from_json(filepath):
-    print("Loading DAG from JSON file " + filepath + "....") #TODO: Custom logging with control of verbosity.
+    '''
+    Loads a DAG from a JSON file.
+
+    Args:
+        filepath (str): Path to the JSON file.
+
+    Returns:
+        nx.DiGraph: Directed acyclic graph loaded from JSON data.
+    '''
+    print("Loading DAG from JSON file " + filepath + "....")
     start_time = timeit.default_timer()
     graph = nx.DiGraph()
     with open(filepath, "r") as file_handle:
         object_data = json.load(file_handle)
-        nodes:dict = object_data["nodes"]
-        #node_indices = [(int(k), {"duration":datetime.strptime(v["Data"][:-1], "%H:%M:%S.%f")} ) for (k,v) in nodes.items()]
-        node_indices = [(int(k), {"duration":timedelta(hours=int(time_parts[0]), minutes=int(time_parts[1]), seconds=float(time_parts[2]))} ) for (k,v) in nodes.items() if (time_parts:=v["Data"].split(':'))]
-        edges = []
-        for (k,v) in nodes.items():
-            for dep in v["Dependencies"]:
-                edges.append((dep, int(k)))
+        nodes = object_data["nodes"]
+        node_indices = [(int(k), {"duration": timedelta(hours=int(v["Data"].split(':')[0]), 
+                                                      minutes=int(v["Data"].split(':')[1]), 
+                                                      seconds=float(v["Data"].split(':')[2]))}) 
+                        for k, v in nodes.items() if ":" in v["Data"]]
+        edges = [(dep, int(k)) for k, v in nodes.items() for dep in v["Dependencies"]]
         graph.add_nodes_from(node_indices)
         graph.add_edges_from(edges)
     elapsed = timeit.default_timer() - start_time
-    print("Loading file took : ", elapsed) # TODO: timeit for JSON file loading
-    
+    print("Loading file took : ", elapsed)
     return graph
-        
+
 @profile    
 def load_dag_from_json_rx(filepath):
-    print("Loading DAG from JSON file " + filepath + "....")  # TODO: Custom logging with control of verbosity.
+    '''
+    Loads a DAG from a JSON file using rustworkx.
+
+    Args:
+        filepath (str): Path to the JSON file.
+
+    Returns:
+        rustworkx.PyDiGraph: Directed acyclic graph loaded from JSON data.
+        dict: Durations of nodes in the graph.
+    '''
+    print("Loading DAG from JSON file " + filepath + "....")
     start_time = timeit.default_timer()
     graph = rx.PyDiGraph()
     durations = {}
@@ -68,7 +98,7 @@ def load_dag_from_json_rx(filepath):
         for node_id, node_data in nodes.items():
             time_parts = node_data["Data"].split(':')
             duration = timedelta(hours=int(time_parts[0]), minutes=int(time_parts[1]), seconds=float(time_parts[2]))
-            durations[int(node_id)] = duration  # Storing duration in the durations dictionary
+            durations[int(node_id)] = duration
             nodes_list.append(int(node_id))
             edges_list += [(dep, int(node_id)) for dep in node_data["Dependencies"]]
     del object_data
@@ -81,17 +111,13 @@ def load_dag_from_json_rx(filepath):
     graph.add_edges_from_no_data(new_edges_list)
     del new_edges_list
     elapsed = timeit.default_timer() - start_time
-    print("Loading file took:", elapsed)  # TODO: timeit for JSON file loading
-
+    print("Loading file took:", elapsed)
     return graph, durations
 
 if __name__ == "__main__":
-
     # Example usage
     num_nodes = 7
     max_duration = 5
 
-    #dag = generate_random_dag(num_nodes, max_duration)
+    # Load a DAG from a JSON file
     dag = load_dag_from_json("./smallComplex.json")
-    # print(nx.get_node_attributes(dag,"duration"))
-    #plot_dag(dag)
